88b6662396a664de67e7a3ab54408669c0253176
RyvynYoung <ryvyn.young@gmail.com>
2023-11-03 23:11:36 -0500

    Initial commit

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/Notes.java
@@ -0,0 +1,19 @@
+package org.firstinspires.ftc.teamcode.drive;
+
+public class Notes {
+    /*
+    for meet one strafe drop yellow use color sensor
+
+    11/2/23 git token acc
+    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
+
+
+
+
+
+     */
+
+
+
+
+}
